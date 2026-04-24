02de97e91c2557fec5725a166d1e4ce14b7c8ace
Ethan Lipnik <hi@ethanlipnik.com>
2026-04-24 03:13:33 -0400

    Update Loom to 1.7.9

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/EthanLipnik/Loom.git", exact: "1.7.8"),
+        .package(url: "https://github.com/EthanLipnik/Loom.git", exact: "1.7.9"),
     ],
     targets: [
         .target(
