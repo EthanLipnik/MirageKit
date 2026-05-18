02c3f0eadb9797a3320e9366069e7e488214601e
Ethan Lipnik <hi@ethanlipnik.com>
2026-05-18 00:48:31 -0700

    Update Loom

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/EthanLipnik/Loom.git", exact: "1.8.11"),
+        .package(url: "https://github.com/EthanLipnik/Loom.git", exact: "1.8.12"),
     ],
     targets: [
         .target(
